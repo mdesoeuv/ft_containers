2e2ae3b4f696dda5543b45add626bff5f037f055
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-04-25 10:24:27 +0200

    ft::stack function prototypes listed

--- srcs/stack.hpp
+++ srcs/stack.hpp
@@ -1,9 +1,64 @@
 #ifndef STACK_HPP
 # define STACK_HPP
 
+# include <deque> // to remove for use with ft::vector
+
 namespace ft
 {
-	template clas
+	template <class T, class Container = std::deque<T> > // change the underlying container by ft::vector
+	class stack
+	{
+
+		using container_type = Container;
+		using value_type = Container::value_type;
+		using size_type = Container::size_type;
+		using reference = Container::reference;
+		using const_reference = Container::const_reference;
+
+	private:
+		
+	protected:
+
+		Container c;
+
+	public:
+
+		explicit stack( const Container& cont = Container() );
+		stack( const stack& other );
+		~stack(void);
+		stack&	operator=(const stack& other);
+
+		T&			top(void); // change T by reference
+		const T&	top(void) const;	// change T by const reference
+
+		bool		empty(void) const;
+		size_type	size(void) const;
+
+		void		push(const value_type& value);
+		void		pop(void);
+
+	};
+	
+	template< class T, class Container >
+	bool operator==(const ft::stack<T, Container>& lhs, const ft::stack<T,Container>& rhs);
+
+	template< class T, class Container >
+	bool operator!=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+
+	template< class T, class Container >
+	bool operator<(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+
+	template< class T, class Container >
+	bool operator<=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+
+	template< class T, class Container >
+	bool operator>(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+
+	template< class T, class Container >
+	bool operator>=(const ft::stack<T, Container>& lhs, const ft::stack<T, Container>& rhs);
+
+
+
 }
 
 #endif
