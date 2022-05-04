f152e3623b3795c9e50b07df3610a912bb8ea60b
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-05-04 15:59:10 +0200

    sub-class value_compare implemented

--- inc/map.hpp
+++ inc/map.hpp
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 14:45:27 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/05/04 14:45:28 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/05/04 15:57:00 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,61 @@
 
 namespace ft
 {
-	
+	template<
+    class Key,
+    class T,
+    class Compare = std::less<Key>,
+    class Allocator = std::allocator<std::pair<const Key, T> >
+	>
+	class map
+	{
+		public:
+
+			/* ----- forward declarations ----- */
+			
+			class Iterator;
+			class Const_Iterator;
+			
+			/* ----- member types ----- */
+
+			typedef Key 											key_type;
+			typedef T												mapped_type;
+			typedef typename ft::pair<const Key, T> 				value_type;		
+			typedef std::size_t										size_type;
+			typedef std::ptrdiff_t									difference_type;
+			typedef Compare											key_compare;
+			typedef Allocator										allocator_type;
+			typedef value_type&										reference;
+			typedef const value_type&								const_reference;
+			typedef typename Allocator::pointer						pointer;
+			typedef typename Allocator::const_pointer				const_pointer;
+			typedef Iterator										iterator;
+			typedef Const_Iterator									const_iterator;
+			typedef typename ft::Reverse_Iterator<Iterator>			reverse_iterator;
+			typedef typename ft::Reverse_Iterator<Const_Iterator>	const_reverse_iterator;
+		
+			/* ----- member classes ----- */
+
+			class value_compare
+			{
+
+				protected:
+				
+					Compare	comp;
+					value_compare(Compare c) : comp(c)
+					{}
+								
+				public:
+				
+					typedef bool		result_type;
+					typedef	value_type	first_argument_type;
+					typedef value_type	second_argument_type;
+
+					bool	operator() (const value_type& lhs, const value_type& rhs) const
+					{
+						return (comp(lhs.first, rhs.first));
+					}
+
+			};
+	};
 }
