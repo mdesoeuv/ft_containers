f943ae37a215020ab1025d3d78cf0b0d3ea3453a
Mehdi Desoeuvre <mehdi.desoeuvre@gmail.com>
2022-05-04 15:01:22 +0200

    lexicographical_compare implemented

--- inc/lexicographical_compare.hpp
+++ inc/lexicographical_compare.hpp
@@ -6,7 +6,7 @@
 /*   By: mdesoeuv <mdesoeuv@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 14:45:16 by mdesoeuv          #+#    #+#             */
-/*   Updated: 2022/05/04 14:45:24 by mdesoeuv         ###   ########lyon.fr   */
+/*   Updated: 2022/05/04 14:59:57 by mdesoeuv         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,32 @@
 
 namespace ft
 {
+	template <class InputIterator1, class InputIterator2>
+  	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+	while (first1 != last1)
+	{
+		if (first2 == last2 || *first2 < *first1)
+			return (false);
+		else if (*first1 < *first2)
+			return (true);
+		++first1;
+		++first2;
+	}
+	return (first2 != last2);
+	}
+	
+	template<class InputIt1, class InputIt2, class Compare>
+	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Compare comp)
+	{
+		for ( ; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2 ) 
+		{
+			if (comp(*first1, *first2))
+				return (true);
+			if (comp(*first2, *first1))
+				return (false);
+		}
+		return ((first1 == last1) && (first2 != last2));
+	}
 	
 }
